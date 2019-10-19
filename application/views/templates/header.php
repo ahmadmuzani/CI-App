0dbe11adb7e06f0a68792063abb37d64ccca74fb
ahmadmuzani <ahmad.muzani@mail.unpas.ac.id>
2019-10-19 09:47:18 +0700

    commit2

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,29 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title><?= $data['judul'] ?></title>
-	<link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?= base_url(); ?>assets/css/style.css">
+  <title><?php echo $judul; ?></title>
 </head>
 <body> 
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 	<div class="container">
-  <a class="navbar-brand" href="<?= BASEURL;?>">CI-App</a>
+  <a class="navbar-brand" href="">CI-App</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="<?= BASEURL;?>">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?= BASEURL;?>/mahasiswa">Mahasiswa</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?= BASEURL;?>/about">About</a>
-      </li>
-    </ul>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+   <div class="navbar">
+        <a class="nav-link" href="<?= base_url(); ?>">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="<?= base_url(); ?>/mahasiswa">Mahasiswa</a>
+        <a class="nav-link" href="<?= base_url(); ?>/about">About</a>
+  </div>
   </div>
 </div>
 </nav> 
